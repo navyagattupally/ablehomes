f61fb3137ca4398f857ac00a3b044a19c9063231
navya <navyagattupally@ymail.com>
2015-11-07 13:39:14 -0800

    Bug Fix
    The server shall run now.
    use node app.js

--- Abled_Homes/config/init/routes.js
+++ Abled_Homes/config/init/routes.js
@@ -4,31 +4,21 @@
 
 */
 
-var userController = require('../../controllers/userController')
-	, categoryController = require('../../controllers/categoryController')
-	, productController=require('../../controllers/productController')
-	, baseurl = '/freedoor/v1'
+var listingsController = require('../../controllers/listingsController')
+	, baseurl = '/abledhomes/v1'
 ;
 
 module.exports = function(app, env) {
-	//set all the routes for freedoor application
-	
-	// User routes
-	app.post(baseurl + '/users', userController.postUser);
-	app.get(baseurl + '/users/:user_id', userController.getUser);
-	
-	// Category routes
-	app.post(baseurl + '/category', categoryController.postCategory);
-	app.get(baseurl + '/category/:category_id', categoryController.getCategory);
-	app.get(baseurl + '/category', categoryController.getCategories);
-	
-	
+
+
+
+
 	//Products routes
-	app.get(baseurl + '/category/:category_id/product', productController.getProducts);
-	app.post(baseurl + '/category/:category_id/product', productController.postProduct);
-	app.get(baseurl + '/category/:category_id/product/:product_id', productController.getProduct);
-	app.put(baseurl + '/category/:category_id/product/:product_id', productController.putProduct);
-	app.delete(baseurl + '/category/:category_id/product/:product_id', productController.deleteProduct);
-	
+	app.get(baseurl + '/listings/:vendor', listingsController.getListings);
+//	app.post(baseurl + '/category/:category_id/product', productController.postProduct);
+//	app.get(baseurl + '/category/:category_id/product/:product_id', productController.getProduct);
+//	app.put(baseurl + '/category/:category_id/product/:product_id', productController.putProduct);
+//	app.delete(baseurl + '/category/:category_id/product/:product_id', productController.deleteProduct);
+
 
 }
